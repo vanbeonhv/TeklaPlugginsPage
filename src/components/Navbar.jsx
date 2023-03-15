5442b26daddacd962b94a353a451d47248753cbd
Marc Nguyen <nguyenvannhv26@gmail.com>
2023-03-15 13:20:00 +0700

    Update download button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,27 +6,27 @@ const Navbar = () => {
     <>
       <header className='header fixed w-full p-3'>
         <nav className='navbar flex justify-between items-center'>
-          <div className='site-logo flex items-center'>
-            <Link to='/'>
+          <Link to='/'>
+            <div className='site-logo flex items-center'>
               <RxCodesandboxLogo className='text-4xl text-bright-blue-500 m-2' />
-            </Link>
 
-            <span className='text-2xl font-bold text-gray-800 cursor-default'>
-              <Link to='/'>Marc</Link>
-              <span className='font-extralight text-gray-600'>Pro</span>
-            </span>
-          </div>
+              <span className='text-2xl font-bold text-gray-800'>
+                Marc
+                <span className='font-extralight text-gray-600'>Pro</span>
+              </span>
+            </div>
+          </Link>
           <div>
             <ul className='flex text-lg text-gray-600 capitalize cursor-pointer font-medium'>
-              <li className='p-2 hover:text-black mr-2'>
-                <Link to='/plugins'>pluggins</Link>
-              </li>
-              <li className='p-2 hover:text-black mr-2'>
-                <Link to='/pricing'>pricing</Link>
-              </li>
-              <li className='p-2 hover:text-black mr-2'>
-                <Link to='/contact'>contact</Link>
-              </li>
+              <Link to='/plugins'>
+                <li className='p-2 hover:text-black mr-2'>plugins</li>
+              </Link>
+              <Link to='/pricing'>
+                <li className='p-2 hover:text-black mr-2'>pricing</li>
+              </Link>
+              <Link to='/contact'>
+                <li className='p-2 hover:text-black mr-2'>contact</li>
+              </Link>
               <button className='px-5 bg-bright-blue-500 text-white rounded-lg font-medium hover:bg-bright-blue-700'>
                 Sign up
               </button>
--- src/pages/Plugins.jsx
+++ src/pages/Plugins.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { HiDownload } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 const Plugins = () => {
   return (
     <div className='w-full '>
       <div className='plugin-header bg-bright-blue-500 py-10 text-center'>
-        <div className='container '>
+        <div className='container mx-auto'>
           <h3 className='text-white text-4xl font-semibold capitalize'>
             Model faster. Drawing faster.
           </h3>
@@ -16,10 +17,10 @@ const Plugins = () => {
       <div className='page-content'>
         <div className='container mx-auto'>
           <div className='overview py-5'>
-            <div className='grid grid-cols-3 gap-4'>
-              <div className='col-12 col-lg-4 border rounded-2xl p-4 shadow-sm  hover:shadow-md hover:bg-bright-blue-100 shadow-sm  hover:shadow-md hover:bg-bright-blue-100'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+              <div className='col-12 col-lg-4 border rounded-2xl p-4 shadow-sm  hover:shadow-md hover:bg-bright-blue-100 hover:shadow-lg hover:bg-bright-blue-100 group'>
                 <div className=' '>
-                  <div className=''>
+                  <div className='relative '>
                     <h5 className=' mb-3 text-2xl font-semibold'>
                       Revit Import
                     </h5>
@@ -28,6 +29,12 @@ const Plugins = () => {
                       Vitae velit inventore quibusdam suscipit nihil labore
                       minus!
                     </div>
+                    <a
+                      href='https://wohhup-my.sharepoint.com/:u:/g/personal/nguyen_huuvan_wohhup_com_vn/Eezo9i0vjNVAjQXyHzGLYCABtqP53KKtlxVdT7XW12w6Qg?e=MFormq'
+                      target='_blank'
+                    >
+                      <HiDownload className='absolute top-0 right-0 text-4xl border rounded-full p-2 text-bright-blue-500 bg-bright-blue-100 group-hover:bg-white' />
+                    </a>
                   </div>
                 </div>
               </div>
