f813d8fce2de18bf8ad108002b052feafe468d94
marc <nguyenvannhv26@gmail.com>
2025-06-07 17:12:22 +0700

    refactor: remove unused imports

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,6 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { DiCodeigniter } from 'react-icons/di';
 import { v4 as uuidv4 } from 'uuid';
-import app from '../../firebase';
 import { useEffect } from 'react';
 import useUserData from '../hooks/useUserData';
 interface IToastMsg {
@@ -16,12 +15,8 @@ interface IToastMsg {
 
 const Msg = ({ closeToast, toastProps, displayName }: IToastMsg) => (
   <p>
-    Hi{' '}
-    <span className='text-bright-blue-500 font-medium capitalize'>
-      {displayName ? displayName : 'bro!'}
-    </span>
-    <DiCodeigniter className='inline-block pb-2 fill-orange-600 text-lg' />,
-    welcome back!
+    Hi <span className='text-bright-blue-500 font-medium capitalize'>{displayName ?? 'bro!'}</span>
+    <DiCodeigniter className='inline-block pb-2 fill-orange-600 text-lg' />, welcome back!
   </p>
 );
 
@@ -35,8 +30,7 @@ const cards = [
   {
     iconName: ICON.FAST,
     header: 'lightning fast',
-    content:
-      'We understand that time is of the essence. So plugins will help us to work faster'
+    content: 'We understand that time is of the essence. So plugins will help us to work faster'
   },
   {
     iconName: ICON.UPDATE,
@@ -68,15 +62,11 @@ function LandingPage() {
       <section className='container'>
         <div className='grid md:grid-cols-2 sm:grid-cols-1'>
           <div className='w-full '>
-            <h1 className='text-6xl font-bold mb-5 mt-10 cursor-default '>
-              Try some useful Tekla plugins
-            </h1>
+            <h1 className='text-6xl font-bold mb-5 mt-10 cursor-default '>Try some useful Tekla plugins</h1>
             <p className='inline-block text-gray-600 font-medium text-lg cursor-default'>
-              Developed with <AiFillHeart className='text-red-600 m-0 inline' />{' '}
-              for <span className='text-bright-blue-500 font-bold'>Tekla</span>{' '}
-              modellers, especially working on{' '}
-              <span className='text-bright-blue-500 font-bold'>PPVC</span>{' '}
-              Project.
+              Developed with <AiFillHeart className='text-red-600 m-0 inline' /> for{' '}
+              <span className='text-bright-blue-500 font-bold'>Tekla</span> modellers, especially working on{' '}
+              <span className='text-bright-blue-500 font-bold'>PPVC</span> Project.
               <br />
               These plugins will help works faster and save a lot of time.
             </p>
@@ -88,41 +78,23 @@ function LandingPage() {
                 children='Download plugins'
                 iconName='link'
               />
-              <Button
-                type='button'
-                btnType='btn-secondary'
-                linkTo=''
-                children='How to install?'
-                iconName='link'
-              />
+              <Button type='button' btnType='btn-secondary' linkTo='' children='How to install?' iconName='link' />
             </div>
           </div>
           <div className='w-full mt-10'>
-            <img
-              src='https://themes.3rdwavemedia.com/coderpro/bs5/2.0/assets/images/promo-figure-alt.svg'
-              alt=''
-            />
+            <img src='https://themes.3rdwavemedia.com/coderpro/bs5/2.0/assets/images/promo-figure-alt.svg' alt='' />
           </div>
         </div>
       </section>
       <section className=' bg-bright-blue-100'>
         <div className='container'>
           <div className='text-center py-14'>
-            <h4 className='text-4xl font-bold capitalize'>
-              made for BIM modellers
-            </h4>
-            <p className='capitalize text-xl text-gray-500 pt-5'>
-              automate your work
-            </p>
+            <h4 className='text-4xl font-bold capitalize'>made for BIM modellers</h4>
+            <p className='capitalize text-xl text-gray-500 pt-5'>automate your work</p>
           </div>
           <div className='flex w-full gap-5 pb-24'>
             {cards.map((card) => (
-              <Card
-                key={uuidv4()}
-                iconName={card.iconName}
-                header={card.header}
-                content={card.content}
-              />
+              <Card key={uuidv4()} iconName={card.iconName} header={card.header} content={card.content} />
             ))}
           </div>
         </div>
